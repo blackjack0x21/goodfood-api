32558161cdeff4530818429435c3fa601914dd1b
Blackjack <bl4ckj4ck.contact@gmail.com>
2022-03-06 00:21:51 +0100

    ajout controllers

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,9 @@
 import express from 'express';
 
+import { getUser } from '../controllers/user.js'
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello');
-});
+router.get('/', getUser);
 
 export default router;
